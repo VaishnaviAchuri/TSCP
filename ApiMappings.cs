a1c5d8cd9dcbff9b7a18981f07db5ad5af01654a
VaishnaviAchuri <94669943+VaishnaviAchuri@users.noreply.github.com>
2021-12-03 13:56:48 +0530

    Add files via upload

---
+++ ApiMappings.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+using Teacher_Student_Connect_Project.Models;
+using Teacher_Student_Connect_Project.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teacher_Student_Connect_Project
+{
+    public class ApiMappings : Profile
+    {
+        public ApiMappings()
+        {
+            CreateMap<User, UserViewModel>().ReverseMap();
+            CreateMap<Document, DocumentViewModel>().ReverseMap();
+            CreateMap<Student, StudentViewModel>().ReverseMap();
+            CreateMap<Teacher, TeacherViewModel>().ReverseMap();
+            CreateMap<Subject, SubjectViewModel>().ReverseMap();
+            CreateMap<Batch, BatchViewModel>().ReverseMap();
+            CreateMap<Category, CategoryViewModel>().ReverseMap();
+            CreateMap<Specialization, SpecializationViewModel>().ReverseMap();
+            CreateMap<Document, AddDocuments>().ReverseMap();
+
+        }
+    }
+}
